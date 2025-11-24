8a610f406da9f0c563b38757e08ef64a5ee11283
almazova09 <almazova1509@gmail.com>
2025-11-24 06:25:57 +0000

    fixed index.js

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 
 
-var api_url = process.env.API_HOST + '/api/status';
+var api_url = process.env.API_URL + '/api/status';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
